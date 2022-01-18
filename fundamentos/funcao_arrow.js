64976bfea460e099c9079a3addb8fe8fe8ab42dd
joaorobsonR <jrobsonramalho@gmail.com>
2022-01-18 18:28:02 -0300

    armazenando função em variável

---
+++ fundamentos/funcao_arrow.js
@@ -0,0 +1,15 @@
+const imprirSoma = function (a, b) {
+    return a + b
+}
+
+const soma = (a, b) => {    //modo simples de criar uma função
+    return a + b
+}
+
+//retorno implicito
+
+const subtrai = (a, b) => a - b //quando o corpo da função só tem uma linha
+
+const imprimi = a => console.log(a)
+
+imprimi('bem bacana')
