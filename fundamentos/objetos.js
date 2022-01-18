51c5d2cc81537af35ac5b2e8e6456384dcaa00f1
joaorobsonR <jrobsonramalho@gmail.com>
2022-01-18 15:43:27 -0300

    basico sobre objeto

---
+++ fundamentos/objetos.js
@@ -0,0 +1,13 @@
+const prod1 = {}  //assim é uma das formas de criar objeto
+prod1.nome = 'caneta'
+prod1.preco = 5.80
+
+console.log(prod1)
+
+const prod2 = {             //segunada forma de criar um objeto
+    nome: 'camisa polo',
+    preco: 20,
+    decontoAvista: 20*0.8
+}
+
+console.log(prod2)
