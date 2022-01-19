ec30278af624f3461c3d41e2c002299ccc0cdc71
joaorobsonR <jrobsonramalho@gmail.com>
2022-01-19 12:31:09 -0300

    usando let e var no loop for

---
+++ fundamentos/usando_var_for.js
@@ -0,0 +1,11 @@
+for(var i = 0; i < 10; i++) {
+    console.log(i)
+}
+
+console.log('i =', i) //incrível como isso pode acontecer
+
+for(let a = 0; a < 10; a++) {
+    console.log(a)
+}
+
+console.log(a) //quando declarado como let a variável a não é definida fora do bloco TOP!
