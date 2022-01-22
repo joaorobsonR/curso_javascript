539d73dd0727efb664be8888f67d488a1fcb0e83
joaorobsonR <jrobsonramalho@gmail.com>
2022-01-22 16:09:41 -0300

    if e valor booleano importante

--- estrutura_controle/if_else.js
+++ estrutura_controle/if_else.js
@@ -1,1 +1,34 @@
-const a = 0
+function soBoaNoticia(nota) {
+    if(nota >= 7) {
+        console.log('Aprovado com ' + nota)
+    }
+}
+
+soBoaNoticia(8.1)
+soBoaNoticia(6.1)
+
+/*
+esse exemplo mostra como uma pecurialidade do javascript pode
+nos auxiliar para obtermos valores expressões logicas, abaixo tem
+alguns exemplos de para obtermos esse valor
+*/
+
+function seForVerdadeEuFalo(valor) {
+    if(valor) {
+        console.log('É verdade... ' + valor)
+    }
+}
+//esse valores retornam false
+seForVerdadeEuFalo()
+seForVerdadeEuFalo(null)
+seForVerdadeEuFalo(undefined)
+seForVerdadeEuFalo(NaN)
+seForVerdadeEuFalo('')
+seForVerdadeEuFalo(0)
+//esse valores são alguns exemplos que retornam verdadeiro
+seForVerdadeEuFalo(-1)
+seForVerdadeEuFalo(' ')
+seForVerdadeEuFalo('?')
+seForVerdadeEuFalo([])
+seForVerdadeEuFalo([1, 2])
+seForVerdadeEuFalo({})
