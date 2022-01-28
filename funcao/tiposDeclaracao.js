c3407d1203f69bf6da8f3fc0f35c5dc8ac53bc94
joaorobsonR <jrobsonramalho@gmail.com>
2022-01-28 15:31:09 -0300

    tipos de declarção e comentários

--- funcao/tiposDeclaracao.js
+++ funcao/tiposDeclaracao.js
@@ -1,4 +1,10 @@
 console.log(soma(3, 4))
+/* 
+ oserva-se que a primeira forma de declarar a função pode ser chamada antes de
+ ser declarada pois o interpretador do java script  primeiro lê as funções,
+ já as outras declarações do não acontece o mesmo
+*/
+
 
 // function declaration
 function soma(x, y) {
