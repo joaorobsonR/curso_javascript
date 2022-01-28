865023b4e89df4cb9d10a5280cda70abb0e076d6
joaorobsonR <jrobsonramalho@gmail.com>
2022-01-28 13:18:38 -0300

    funão callback executada no browser

---
+++ funcao/callBack3.js
@@ -0,0 +1,7 @@
+// função executada no browser
+
+document.getElementsByTagName('body')[0].onclick = function (e) {
+    console.log('ocorreu um erro')
+}
+
+//não é preciso declara um evento na função, mas pode deixar de padrão
