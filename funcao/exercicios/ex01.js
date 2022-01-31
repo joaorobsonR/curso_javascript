b4c0e15bc79b754f5722185202bb5c5d0950dc4d
joaorobsonR <jrobsonramalho@gmail.com>
2022-01-31 19:47:30 -0300

    ex 01 função arrow

---
+++ funcao/exercicios/ex01.js
@@ -0,0 +1,13 @@
+const soma = (n1, n2) => n1 + n2
+
+const sub = (n1, n2) => n1 - n2
+
+const mult = (n1, n2) => n1 * n2
+
+const div = (n1, n2) => n1 / n2
+
+const mod = (n1, n2) => n1 % n2
+
+console.log(soma(100, 25), sub(100, 25), mult(100, 25), div(100, 25), mod(100, 25))
+
+//foi utilizado a função arrow 
