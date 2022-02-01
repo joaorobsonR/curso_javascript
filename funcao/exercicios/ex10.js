bbd21db30c1503ea4175be2320c576f2d09469dc
joaorobsonR <jrobsonramalho@gmail.com>
2022-02-01 19:47:02 -0300

    verificar se numero é divisivel por tres

---
+++ funcao/exercicios/ex10.js
@@ -0,0 +1,16 @@
+const verifica = num => {
+    num = num.toString()
+    num.split(',')
+    let soma = 0
+    for(let i = 0; i < num.length; i++) {
+        soma += num[i]
+    }
+    if(soma % 3 === 0) {
+        return true
+    } else {
+        return false
+    }
+}
+
+console.log(verifica(45))
+
