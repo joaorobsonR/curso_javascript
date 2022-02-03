cf31e992cd031fd263378677a4a183acc038a535
joaorobsonR <jrobsonramalho@gmail.com>
2022-02-03 12:16:37 -0300

    loja de carros

--- funcao/exercicios/ex16.js
+++ funcao/exercicios/ex16.js
@@ -1,3 +1,17 @@
-function carrosdaLoja() {
-    
+function carrosdaLoja(carro) {
+    switch(carro) {
+        case 'hatch':
+            console.log('compra efetuada com sucesso')
+            break
+
+        case 'sedan': case 'motocicleta': case 'caminhonete':
+            console.log('tem certeza que prefere esse modelo')
+            break
+
+        default:
+            console.log('não trabalhamos com esse modelo')
+
+    }
 }
+
+carrosdaLoja('hatch')
